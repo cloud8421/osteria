1cbf5726b119823b52d955d5c5731e7e2042957b
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2016-09-05 13:42:56 +0200

    Dynamically generate tables

--- lib/osteria.ex
+++ lib/osteria.ex
@@ -6,16 +6,13 @@ defmodule Osteria do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    tables = Enum.map((1..10), fn(i) ->
+      worker(Osteria.Table, [i, Enum.random((1..10))], id: i)
+    end)
+
     # Define workers and child supervisors to be supervised
     children = [
       worker(Osteria.TableMap, []),
-      worker(Osteria.Table, [1, 3], id: 1),
-      worker(Osteria.Table, [2, 4], id: 2),
-      worker(Osteria.Table, [3, 7], id: 3),
-      worker(Osteria.Table, [4, 2], id: 4),
-      worker(Osteria.Table, [5, 3], id: 5),
-      worker(Osteria.Table, [6, 4], id: 6),
-      worker(Osteria.Table, [7, 2], id: 7),
       worker(Osteria.Waiter, []),
       worker(Osteria.Chef, []),
       worker(Osteria.LineCook, [:grill], id: :grill),
@@ -24,7 +21,7 @@ defmodule Osteria do
       worker(Osteria.LineCook, [:oven], id: :oven)
       # Starts a worker by calling: Osteria.Worker.start_link(arg1, arg2, arg3)
       # worker(Osteria.Worker, [arg1, arg2, arg3]),
-    ]
+    ] ++ tables
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
@@ -33,5 +30,8 @@ defmodule Osteria do
             max_seconds: 5,
             name: Osteria.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp generate_tables(total) do
   end
 end
