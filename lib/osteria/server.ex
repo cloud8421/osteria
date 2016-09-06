9f52de3618c4186d76e7e9545c16358d9ea923bd
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2016-09-06 12:47:37 +0200

    Serialise status response

--- lib/osteria/server.ex
+++ lib/osteria/server.ex
@@ -5,10 +5,32 @@ defmodule Osteria.Server do
   plug :dispatch
 
   get "/status" do
-    send_resp(conn, 200, Poison.encode!(Osteria.Status.get))
+    body = Osteria.Status.get
+           |> serialize_status
+           |> Poison.encode!
+    send_resp(conn, 200, body)
   end
 
   match _ do
     send_resp(conn, 404, "not found")
+  end
+
+  defp serialize_status(status) do
+    %{tables: Map.values(status.tables),
+      chef: serialize_chef(status.chef),
+      line_cooks: serialize_line_cooks(status.line_cooks)}
+  end
+
+  defp serialize_chef([chef_status]) do
+    %{table_number: chef_status.table_number,
+      orders: Map.get(chef_status, :to_prepare)}
+  end
+
+  defp serialize_line_cooks(line_cooks) do
+    Enum.reduce(line_cooks, [], fn({area, dishes}, acc) ->
+      dish_names = Enum.map(dishes, &(&1.name))
+      entry = %{area: area, dishes: dish_names}
+      [entry | acc]
+    end)
   end
 end
