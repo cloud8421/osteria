396a0a4a70230162758b166a629724a163f8d26d
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2016-09-15 15:22:58 +0100

    Serve static index at root

--- lib/osteria/server.ex
+++ lib/osteria/server.ex
@@ -6,6 +6,12 @@ defmodule Osteria.Server do
     from: "frontend/build"
 
   plug :match
+  plug :dispatch
+
+  get "/" do
+    body = File.read!("frontend/build/index.html")
+    send_resp(conn, 200, body)
+  end
 
   match _ do
     send_resp(conn, 404, "not found")
